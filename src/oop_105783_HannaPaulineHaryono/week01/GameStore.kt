bb778213a08e574b77dd636215c213721dac9677
nna <hannapauline16@gmail.com>
2026-09-05 10:55:32 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_105783_HannaPaulineHaryono/week01/GameStore.kt
+++ src/oop_105783_HannaPaulineHaryono/week01/GameStore.kt
@@ -1,4 +1,14 @@
 package oop_105783_HannaPaulineHaryono.week01
 
-class GameStore {
+fun main() {
+    val gameTitle: String = "Mario Kart"
+    val price: Int = 600000
+
+    val discount = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    (price * 0.20).toInt()
+} else {
+    (price * 0.10).toInt()
 }
