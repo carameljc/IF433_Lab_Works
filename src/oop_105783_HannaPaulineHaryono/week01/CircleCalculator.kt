29ec877b8aede9c1b012accaa02472d9b32870bf
nna <hannapauline16@gmail.com>
2026-09-05 10:38:18 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_105783_HannaPaulineHaryono/week01/CircleCalculator.kt
+++ src/oop_105783_HannaPaulineHaryono/week01/CircleCalculator.kt
@@ -1,4 +1,23 @@
 package oop_105783_HannaPaulineHaryono.week01
 
-class CircleCalculator {
+fun main(args: Array<String>) {
+// Variable definition
+    val radius = 7.0
+    val pi = 3.14
+// Calculation
+    val area: Double = pi * radius * radius
+
+    // Output Concatenation
+    println("Radius: $radius, Area: $area")
+
+    // Logic check
+    checkSize(area)
+}
+
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
 }
---
+++ src/oop_105783_HannaPaulineHaryono/week02/Student.kt
@@ -0,0 +1,7 @@
+package oop_105783_HannaPaulineHaryono.week02
+
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String,) {
+}
