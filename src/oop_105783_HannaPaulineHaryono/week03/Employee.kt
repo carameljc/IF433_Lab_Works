38122e99b766a57607a8ddd0486e7f449b01c7cd
nna <hannapauline16@gmail.com>
2026-09-12 10:21:30 +0700

    week03: fix setter using backing field

--- src/oop_105783_HannaPaulineHaryono/week03/Employee.kt
+++ src/oop_105783_HannaPaulineHaryono/week03/Employee.kt
@@ -1,5 +1,13 @@
-package week03
+package oop_105783_HannaPaulineHaryono.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
+        }
 }
--- src/oop_105783_HannaPaulineHaryono/week03/Main.kt
+++ src/oop_105783_HannaPaulineHaryono/week03/Main.kt
@@ -1,4 +1,7 @@
 package oop_105783_HannaPaulineHaryono.week03
 
-class Main {
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Program akan crash dengan StackOverflowError
 }
