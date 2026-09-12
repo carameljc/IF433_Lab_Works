7df424d1eb83dc5721b5ed344d6008b16104ba9f
nna <hannapauline16@gmail.com>
2026-09-12 10:29:18 +0700

    week03: test all encapsulation logic in main

--- src/oop_105783_HannaPaulineHaryono/week03/Employee.kt
+++ src/oop_105783_HannaPaulineHaryono/week03/Employee.kt
@@ -13,6 +13,9 @@ class Employee(val name: String) {
 
     private var performanceRating: Int = 3
 
+    val tax: Double
+        get() = salary * 0.1
+
     fun increasePerformance() {
         performanceRating++
         println("Kinerja $name meningkat! Rating: $performanceRating")
--- src/oop_105783_HannaPaulineHaryono/week03/Main.kt
+++ src/oop_105783_HannaPaulineHaryono/week03/Main.kt
@@ -2,6 +2,15 @@ package oop_105783_HannaPaulineHaryono.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000 // Program akan crash dengan StackOverflowError
+
+    // 1. Test Validasi Salary
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    // 2. Test Encapsulation
+    e.increasePerformance()
+
+    // 3. Test Computed Property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
